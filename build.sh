50f8df00afc85bc0bb7d0b9c3321251f3169d3d9
Yoshio Hisadomi <yururelaxbody@docomo.ne.jp>
2024-07-29 15:05:44 +0900

    edit build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@ set -x
 
 
 
-if [ "$1" == "clean" ]; then
+if [ "$1" = "clean" ]; then
     make clean
-elif [ "$1" == "all" ]; then
+elif [ "$1" = "all" ]; then
     make all
-elif [ "$1" == "check" ]; then
+elif [ "$1" = "check" ]; then
     ps -ax | grep ruralis
-elif [ "$1" == "net" ]; then
+elif [ "$1" = "net" ]; then
     netstat -an | grep 158
-elif [ "$1" == "run" ]; then
+elif [ "$1" = "run" ]; then
     nohup ./ruralis > log.log 2>&1 &
-elif [ "$1" == "log" ]; then
+elif [ "$1" = "log" ]; then
     tail -f log.log
 else
     make all
